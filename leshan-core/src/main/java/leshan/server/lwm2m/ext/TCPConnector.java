441ca03ee7d282d70314aec1d5a0b08e49a3d6d2
xmx0632 <xmx0632@gmail.com>
2014-10-23 23:49:00 +0800

    java.lang.IllegalArgumentException: URI port option must be between 0 and 65535 (2 bytes) inclusive but was 1835126237 	at org.eclipse.californium.core.coap.OptionSet.setURIPort(OptionSet.java:292)

--- leshan-core/src/main/java/leshan/server/lwm2m/ext/TCPConnector.java
+++ leshan-core/src/main/java/leshan/server/lwm2m/ext/TCPConnector.java
@@ -1,12 +1,12 @@
 package leshan.server.lwm2m.ext;
 
-
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
-import java.util.Arrays;
-import java.util.LinkedList;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -25,118 +25,108 @@ import org.eclipse.californium.elements.RawDataChannel;
  * {@link RawDataChannel}. An <code>Endpoint</code> sends messages encapsulated
  * within a {@link RawData} by calling the method {@link #send(RawData)} on the
  * connector. When the connector receives a message, it invokes
- * {@link RawDataChannel#receiveData(RawData)}.
- * // TODO: describe that we can make many threads
+ * {@link RawDataChannel#receiveData(RawData)}. // TODO: describe that we can
+ * make many threads
  */
 public class TCPConnector implements Connector {
 
 	public final static Logger LOGGER = Logger.getLogger(TCPConnector.class.toString());
-	
+
 	public static final int UNDEFINED = 0;
-	
+
 	private boolean running;
-	
-	private DatagramSocket socket;
-	
+
+	// private DatagramSocket socket;
+
+	private ServerSocket tcpServerSocket;
+
 	private final InetSocketAddress localAddr;
-	
+
 	private List<Thread> receiverThreads;
 	private List<Thread> senderThreads;
 
 	/** The queue of outgoing block (for sending). */
 	private final BlockingQueue<RawData> outgoing; // Messages to send
-	
+
 	/** The receiver of incoming messages */
 	private RawDataChannel receiver; // Receiver of messages
-	
+
 	private int receiveBuffer = UNDEFINED;
 	private int sendBuffer = UNDEFINED;
-	
+
 	private int senderCount = 1;
 	private int receiverCount = 1;
-	
+
 	private int receiverPacketSize = 2048;
 	private boolean logPackets = false;
-	
+
 	public TCPConnector() {
 		this(new InetSocketAddress(0));
 	}
-	
+
 	public TCPConnector(InetSocketAddress address) {
 		this.localAddr = address;
 		this.running = false;
-		
+
 		this.outgoing = new LinkedBlockingQueue<RawData>();
 	}
-	
+
 	@Override
 	public synchronized void start() throws IOException {
-		if (running) return;
-		
+		if (running)
+			return;
+
 		// if localAddr is null or port is 0, the system decides
-		socket = new DatagramSocket(localAddr.getPort(), localAddr.getAddress());
-
+		tcpServerSocket = new ServerSocket(localAddr.getPort());
 		this.running = true;
-		
-		if (receiveBuffer != UNDEFINED)
-			socket.setReceiveBufferSize(receiveBuffer);
-		receiveBuffer = socket.getReceiveBufferSize();
-		
-		if (sendBuffer != UNDEFINED)
-			socket.setSendBufferSize(sendBuffer);
-		sendBuffer = socket.getSendBufferSize();
-		
-		// start receiver and sender threads
-		LOGGER.config("TCP-connector starts "+senderCount+" sender threads and "+receiverCount+" receiver threads");
-		
-		receiverThreads = new LinkedList<Thread>();
-		for (int i=0;i<receiverCount;i++) {
-			receiverThreads.add(new Receiver("TCP-Receiver "+localAddr+"["+i+"]"));
-		}
-		
-		senderThreads = new LinkedList<Thread>();
-		for (int i=0;i<senderCount;i++) {
-			senderThreads.add(new Sender("TCP-Sender "+localAddr+"["+i+"]"));
-		}
+		while (true) {
+			Socket connectionSocket = tcpServerSocket.accept();
+			System.out.println("new client connecting");
 
-		for (Thread t:receiverThreads)
-			t.start();
-		for (Thread t:senderThreads)
-			t.start();
-		
-		/*
-		 * Java bug: sometimes, socket.getReceiveBufferSize() and
-		 * socket.setSendBufferSize() block forever when called here. When
-		 * called up there, it seems to work. This issue occurred in Java
-		 * 1.7.0_09, Windows 7.
-		 */
-		
-		LOGGER.config("TCP connector listening on "+socket.getLocalSocketAddress()+", recv buf = "+receiveBuffer+", send buf = "+sendBuffer
-				+", recv packet size = " +receiverPacketSize+  ", log packets = "+logPackets);
+			try {
+				Receiver receiver = new Receiver("TCP-Receiver " + localAddr, connectionSocket);
+				Sender sender = new Sender("TCP-Sender " + localAddr, connectionSocket);
+				receiver.start();
+				sender.start();
+
+				receiver.join();
+				sender.join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
 	}
 
 	@Override
 	public synchronized void stop() {
-		if (!running) return;
+		if (!running)
+			return;
 		this.running = false;
 		// stop all threads
-		if (senderThreads!= null)
-			for (Thread t:senderThreads)
+		if (senderThreads != null)
+			for (Thread t : senderThreads)
 				t.interrupt();
-		if (receiverThreads!= null)
-			for (Thread t:receiverThreads)
+		if (receiverThreads != null)
+			for (Thread t : receiverThreads)
 				t.interrupt();
 		outgoing.clear();
-		if (socket != null)
-			socket.close();
-		socket = null;
+
+		try {
+			if (tcpServerSocket != null) {
+				tcpServerSocket.close();
+			}
+		} catch (IOException e) {
+		}
+		tcpServerSocket = null;
 	}
 
 	@Override
 	public synchronized void destroy() {
 		stop();
 	}
-	
+
 	@Override
 	public void send(RawData msg) {
 		if (msg == null)
@@ -148,138 +138,148 @@ public class TCPConnector implements Connector {
 	public void setRawDataReceiver(RawDataChannel receiver) {
 		this.receiver = receiver;
 	}
-	
+
 	public InetSocketAddress getAddress() {
-		if (socket == null) return localAddr;
-		else return new InetSocketAddress(socket.getLocalAddress(), socket.getLocalPort());
+		if (tcpServerSocket == null)
+			return localAddr;
+		else
+			return new InetSocketAddress(tcpServerSocket.getInetAddress(), tcpServerSocket.getLocalPort());
 	}
-	
+
 	private abstract class Worker extends Thread {
 
 		/**
 		 * Instantiates a new worker.
 		 *
-		 * @param name the name
+		 * @param name
+		 *            the name
 		 */
 		private Worker(String name) {
 			super(name);
 			setDaemon(true);
 		}
 
-		/* (non-Javadoc)
+		/*
+		 * (non-Javadoc)
+		 * 
 		 * @see java.lang.Thread#run()
 		 */
 		public void run() {
-			LOGGER.config("Start "+getName());
+			LOGGER.config("Start " + getName());
 			while (running) {
 				try {
 					work();
 				} catch (Throwable t) {
 					if (running)
-						LOGGER.log(Level.WARNING, "Exception \""+t+"\" in thread " + getName()+": running="+running, t);
+						LOGGER.log(Level.WARNING, "Exception \"" + t + "\" in thread " + getName() + ": running=" + running, t);
 					else
-						LOGGER.info(getName()+" has successfully stopped");
+						LOGGER.info(getName() + " has successfully stopped");
 				}
 			}
 		}
 
 		/**
-		 * @throws Exception the exception to be properly logged
+		 * @throws Exception
+		 *             the exception to be properly logged
 		 */
 		protected abstract void work() throws Exception;
 	}
-	
-	private class Receiver extends Worker {
-		
-		private DatagramPacket datagram;
-		private int size;
-		
-		private Receiver(String name) {
-			super(name);
-			this.size = receiverPacketSize;
-			this.datagram = new DatagramPacket(new byte[size], size);
-		}
-		
-		protected void work() throws IOException {
-			datagram.setLength(size);
-			socket.receive(datagram);
-			if (logPackets)
-				LOGGER.fine("Connector ("+socket.getLocalSocketAddress()+") received "+datagram.getLength()+" bytes from "+datagram.getAddress()+":"+datagram.getPort());
 
-			byte[] bytes = Arrays.copyOfRange(datagram.getData(), datagram.getOffset(), datagram.getLength());
+	private class Receiver extends Worker {
+		private Socket clientConnection;
+
+		private Receiver(String name, Socket clientConnection) {
+			super(name);
+			this.clientConnection = clientConnection;
+		}
+
+		protected void work() throws IOException {
+
+			byte[] bytes = input2byte(clientConnection.getInputStream());
 			RawData msg = new RawData(bytes);
-			msg.setAddress(datagram.getAddress());
-			msg.setPort(datagram.getPort());
-			
+			InetAddress inetAddress = clientConnection.getInetAddress();
+			System.out.println("inetAddress:" + inetAddress);
+			msg.setAddress(inetAddress);
+			int port = clientConnection.getPort();
+			System.out.println("ppp:" + port);
+			msg.setPort(port);
+
 			receiver.receiveData(msg);
 		}
-		
-	}
-	
-	private class Sender extends Worker {
-		
-		private DatagramPacket datagram;
-		
-		private Sender(String name) {
-			super(name);
-			this.datagram = new DatagramPacket(new byte[0], 0);
+
+		public byte[] input2byte(InputStream inStream) throws IOException {
+			int count = 0;
+			while (count == 0) {
+				count = inStream.available();
+			}
+			byte[] b = new byte[count];
+			inStream.read(b);
+			// inStream.close();
+			return b;
 		}
-		
+	}
+
+	private class Sender extends Worker {
+
+		private Socket clientConnection;
+
+		private Sender(String name, Socket clientConnection) {
+			super(name);
+			this.clientConnection = clientConnection;
+		}
+
 		protected void work() throws InterruptedException, IOException {
 			RawData raw = outgoing.take(); // Blocking
-			datagram.setData(raw.getBytes());
-			datagram.setAddress(raw.getAddress());
-			datagram.setPort(raw.getPort());
-			if (logPackets)
-				LOGGER.fine("Connector ("+socket.getLocalSocketAddress()+") sends "+datagram.getLength()+" bytes to "+datagram.getSocketAddress());
-			socket.send(datagram);
+			byte[] bytes = raw.getBytes();
+			OutputStream outputStream = clientConnection.getOutputStream();
+			outputStream.write(bytes);
 		}
 	}
-	
+
 	public void setReceiveBufferSize(int size) {
 		this.receiveBuffer = size;
 	}
-	
+
 	public int getReceiveBufferSize() {
 		return receiveBuffer;
 	}
-	
+
 	public void setSendBufferSize(int size) {
 		this.sendBuffer = size;
 	}
-	
+
 	public int getSendBufferSize() {
 		return sendBuffer;
 	}
-	
+
 	public void setReceiverThreadCount(int count) {
 		this.receiverCount = count;
 	}
-	
+
 	public int getReceiverThreadCount() {
 		return receiverCount;
 	}
-	
+
 	public void setSenderThreadCount(int count) {
 		this.senderCount = count;
 	}
-	
+
 	public int getSenderThreadCount() {
 		return senderCount;
 	}
-	
+
 	public void setReceiverPacketSize(int size) {
 		this.receiverPacketSize = size;
 	}
-	
+
 	public int getReceiverPacketSize() {
 		return receiverPacketSize;
 	}
-	
+
 	public void setLogPackets(boolean b) {
 		this.logPackets = b;
 	}
-	
+
 	public boolean isLogPackets() {
 		return logPackets;
 	}
